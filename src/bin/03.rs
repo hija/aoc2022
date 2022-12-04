1b52ca0d672e2fded20b7a5f80ee509f56cc7be5
Hilko Janßen <hilko.j@wwu.de>
2022-12-04 16:36:04 +0100

    03_02

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,14 +1,25 @@
-fn get_priority(rucksack: &str) -> u32 {
-    let size_compartments = rucksack.len() / 2;
-    let (first_compartment, second_compartmentt) = rucksack.split_at(size_compartments);
+fn get_priority_for_character(c: char) -> u32 {
+    if c.is_lowercase() {
+        return c as u32 - 96;
+    } else {
+        return (c as u32 - 64) + 26;
+    }
+}
 
-    for c in first_compartment.chars() {
-        if second_compartmentt.contains(c) {
-            if c.is_lowercase() {
-                return c as u32 - 96;
-            } else {
-                return (c as u32 - 64) + 26;
+fn get_priority(data: Vec<&str>) -> u32 {
+    let first_vec = data.first().copied().unwrap();
+
+    for c in first_vec.chars() {
+        let mut is_common_letter = true;
+        for other_vector in data.iter().copied() {
+            if !other_vector.contains(c) {
+                is_common_letter = false;
+                break;
             }
+        }
+
+        if is_common_letter {
+            return get_priority_for_character(c);
         }
     }
 
@@ -17,12 +28,29 @@ fn get_priority(rucksack: &str) -> u32 {
 
 pub fn part_one(input: &str) -> Option<u32> {
     let rucksacks = input.lines();
-    let priorities = rucksacks.map(|x| get_priority(x));
-    return Some(priorities.sum());
+    let mut sum_priorities = 0;
+    for rucksack in rucksacks {
+        let (p1, p2) = rucksack.split_at(rucksack.len() / 2);
+        sum_priorities += get_priority(vec![p1, p2]);
+    }
+    return Some(sum_priorities);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut lines = input.lines().into_iter();
+    let mut sum_priorities = 0;
+    loop {
+        let p1 = lines.next();
+        let p2 = lines.next();
+        let p3 = lines.next();
+
+        if p1.is_none() {
+            break;
+        }
+
+        sum_priorities += get_priority(vec![p1.unwrap(), p2.unwrap(), p3.unwrap()]);
+    }
+    return Some(sum_priorities);
 }
 
 fn main() {
