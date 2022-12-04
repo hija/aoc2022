bd155ac3c5062f47cbe45e6ec1339966ded33fb1
Hilko Janßen <hilko.j@wwu.de>
2022-12-04 16:40:19 +0100

    bugfix test (add expected value)

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -72,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(70));
     }
 }
